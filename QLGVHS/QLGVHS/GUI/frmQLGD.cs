15d18dd378a737c8e569e79bfb771bea38c017bd
dudubeo <dung984730@gmail.com>
2017-03-19 21:22:30 +0700

    dũng quản lí giảng dạy
    lần 1

--- QLGVHS/QLGVHS/GUI/frmQLGD.cs
+++ QLGVHS/QLGVHS/GUI/frmQLGD.cs
@@ -10,10 +10,12 @@ using System.Windows.Forms;
 using QLGCHS_ValueObject;
 using QLGVHS_BUS;
 using QLGVHS_DATA;
+using System.Data.SqlClient;
 namespace QLGVHS.GUI
 {
     public partial class frmQLGD : Form
     {
+        DataTable dt, tb;
         private SQL_tblGiangday DAL_Giang = new SQL_tblGiangday();
         private SQL_tblGiaovien DAL_GV = new SQL_tblGiaovien();
         private SQL_tblLop DAL_Lop = new SQL_tblLop();
@@ -28,8 +30,8 @@ namespace QLGVHS.GUI
         private void frmQLGD_Load(object sender, EventArgs e)
         {
             var source = new AutoCompleteStringCollection();
-
-            DataTable tb = DAL_GV.getThongTinGV();
+            
+             tb = DAL_GV.getThongTinGV();
             cboGiaoVien.DataSource = tb;
             cboGiaoVien.DisplayMember = "HoTen";
             cboGiaoVien.ValueMember = "MaGV";
@@ -42,8 +44,9 @@ namespace QLGVHS.GUI
             cboLop.ValueMember = "MaLop";
             for (int _i = 0; _i < tb.Rows.Count; _i++) source.Add(tb.Rows[_i]["TenLop"].ToString());
             cboLop.AutoCompleteCustomSource = source;
-
-            dgvDanhSach.DataSource = DAL_Giang.getThongTinGD();
+            dt = DAL_Giang.getThongTinGD();
+            dgvDanhSach.DataSource = dt;
+             
         }
 
         private void dgvDanhSach_CellEnter(object sender, DataGridViewCellEventArgs e)
@@ -109,32 +112,84 @@ namespace QLGVHS.GUI
         }
         private void btnThem_Click(object sender, EventArgs e)
         {
+            MoBtn();
+            btnSua.Enabled = false;
+            btnXoa.Enabled = false;
 
+            ResetData();
+
+            _them = true;
         }
 
         private void btnSua_Click(object sender, EventArgs e)
         {
-
+            txtTiet.Enabled = true;
+            btnLuu.Enabled = true;
+            dgvDanhSach.Enabled = false;
+            btnThem.Enabled = false;
+            btnXoa.Enabled = false;
         }
 
         private void btnXoa_Click(object sender, EventArgs e)
         {
 
+            EC_Giang.Tietday = txtTiet.Text;
+            EC_Giang.Thu = cmbThu.Text;
+            EC_Giang.MaLop = cboLop.SelectedValue.ToString();
+            EC_Giang.MaGV = cboGiaoVien.SelectedValue.ToString();
+            DAL_Giang.delGiangday(EC_Giang);
+            Reset();
         }
 
         private void btnLuu_Click(object sender, EventArgs e)
         {
+            if (cboGiaoVien.Text != "" && cboGiaoVien.SelectedValue != null) EC_Giang.MaGV = cboGiaoVien.SelectedValue.ToString();
+            else
+            {
+                MessageBox.Show("Giáo viên không xác định");
+                cboGiaoVien.Focus();
+                return;
+            }
+            if (cboLop.Text != "" && cboLop.SelectedValue != null) EC_Giang.MaLop = cboLop.SelectedValue.ToString();
+            else
+            {
+                MessageBox.Show("Lớp không xác định");
+                cboLop.Focus();
+                return;
+            }
+            EC_Giang.Thu = cmbThu.Text;
+            EC_Giang.Tietday = txtTiet.Text;
 
+            if (_them)
+            {
+                //them tt
+                DAL_Giang.addGiangday(EC_Giang);
+
+                KhoaBtn();
+                Reset();
+            }
+            else
+            {
+                //sua tt
+                DAL_Giang.updateGiangday(EC_Giang);
+                KhoaBtn();
+                Reset();
+            }
+            btnThem.Enabled = true;
+            btnSua.Enabled = true;
+            btnXoa.Enabled = true;
         }
 
         private void txtTimGV_TextChanged(object sender, EventArgs e)
         {
-
+            string st = string.Format("[HoTen] like '%{0}%'", txtTimGV.Text);
+            dt.DefaultView.RowFilter = st;
         }
 
         private void txtTimLop_TextChanged(object sender, EventArgs e)
         {
-
+            string st = string.Format("[TenLop] like '%{0}%'", txtTimLop.Text);
+            dt.DefaultView.RowFilter = st;
         }
 
         private void frmQLGD_FormClosed(object sender, FormClosedEventArgs e)
@@ -142,6 +197,16 @@ namespace QLGVHS.GUI
         //    frmMain frm = new frmMain();
         //    frm.Show();
         //    this.Dispose();
+        }
+
+        private void txtTimGV_Click(object sender, EventArgs e)
+        {
+            txtTimGV.Text = "";
+        }
+
+        private void txtTimLop_Click(object sender, EventArgs e)
+        {
+            txtTimLop.Text = "";
         }
     }
 }
